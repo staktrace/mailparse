812c2776ca90e32e2d4f3d1d7a40299af2b749b8
Kartikaya Gupta <kgupta@mozilla.com>
2016-06-18 17:57:49 +0100

    Add generated documentation

---
+++ target/doc/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['mailparse'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='mailparse/enum.MailParseError.html' title='mailparse::MailParseError'>MailParseError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['mailparse'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='mailparse/enum.MailParseError.html' title='mailparse::MailParseError'>MailParseError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
