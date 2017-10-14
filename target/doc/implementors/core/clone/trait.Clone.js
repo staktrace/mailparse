24a48fbc89c4aedbd3b1a7fd785e0e9c226174ba
Kartikaya Gupta <kats@seldon.staktrace.com>
2017-10-14 00:07:21 -0400

    Regenerate documentation for 0.6.0

---
+++ target/doc/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mailparse"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"mailparse/enum.DispositionType.html\" title=\"enum mailparse::DispositionType\">DispositionType</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
