92aa04b62950754d1b1da7682e022ab0154e3557
Przeglo <przeglo@gmail.com>
2019-08-01 08:21:03 +0200

    Integrate fuzzer so anyone can fuzz easily

---
+++ fuzz/fuzz_targets/fuzz_parse.rs
@@ -0,0 +1,12 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate mailparse;
+use mailparse::*;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(parsed) = parse_mail(data){
+        if let Ok(Some(date)) = parsed.headers.get_first_value("Date") {
+            let _ = dateparse(&date);
+        }
+    }
+});
