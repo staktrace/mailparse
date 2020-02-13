staktrace/mailparse

commit d10c28b41ef1b81d03875043ae304f67b3a80549

MailHeader::get_value: do not add space if special char parsed

src/lib.rs
@@ -241,7 +241,10 @@ impl<'a> MailHeader<'a> {
                                         continue;
                                     }
                                     match self.decode_word(&line[ix_begin..ix_end]) {
-                                        Some(v) => result.push_str(&v),
+                                        Some(v) => {
+                                            result.push_str(&v);
+                                            add_space = false;
+                                        }
                                         None => result.push_str(&line[ix_begin - 2..ix_end + 2]),
                                     };
                                     ix_search = ix_end;
@@ -928,7 +931,7 @@ mod tests {
                                         =?iso-8859-1?Q?_se=F1or!?=",
         ).unwrap();
         assert_eq!(parsed.get_key().unwrap(), "Subject");
-        assert_eq!(parsed.get_value().unwrap(), "\u{a1}Hola,  se\u{f1}or!");
+        assert_eq!(parsed.get_value().unwrap(), "\u{a1}Hola, se\u{f1}or!");
 
         let (parsed, _) = parse_header(b"Euro: =?utf-8?Q?=E2=82=AC?=").unwrap();
         assert_eq!(parsed.get_key().unwrap(), "Euro");
@@ -967,7 +970,7 @@ mod tests {
         assert_eq!(parsed.get_key().unwrap(), "Subject");
         assert_eq!(
             parsed.get_value().unwrap(),
-            "[Ontario Builder] Understanding home shopping \u{2013} a q uick survey"
+            "[Ontario Builder] Understanding home shopping \u{2013} a quick survey"
         );
 
         let (parsed, _) = parse_header(

commit 499721016695c0303aa8cb27930c9b292fe98426

Fix line-unwrapping case where a qp word is followed by additional non-qp stuff on the line.
Found by code inspection.
Fixes defect introduced in #21.

src/lib.rs
@@ -228,7 +228,16 @@ impl<'a> MailHeader<'a> {
                                     match self.decode_word(&line[ix_begin..ix_end]) {
                                         Some(v) => {
                                             result.push_str(&v);
-                                            add_space = false;
+                                            // Drop the space inserted when merging a multiline header, if the
+                                            // quoted-printable word is the last non-whitespace thing on the line.
+                                            // Technically the first clause of the following `if` condition is
+                                            // redundant with the second one, but it allows short-circuiting the
+                                            // overhead of `trim_end` in the common case.
+                                            if ix_end + 2 == line.len()
+                                                || ix_end + 2 == line.trim_end().len()
+                                            {
+                                                add_space = false;
+                                            }
                                         }
                                         None => result.push_str(&line[ix_begin - 2..ix_end + 2]),
                                     };
@@ -990,6 +999,26 @@ mod tests {
         assert_eq!(
             parsed.get_value().unwrap(),
             "[Ontario Builder] Understanding home shopping \u{2013} a quick survey"
+        );
+
+        let (parsed, _) = parse_header(
+            b"Subject: =?utf-8?q?=5BOntario_Builder=5D?= non-qp words\n \
+             and the subject continues",
+        )
+        .unwrap();
+        assert_eq!(
+            parsed.get_value().unwrap(),
+            "[Ontario Builder] non-qp words and the subject continues"
+        );
+
+        let (parsed, _) = parse_header(
+            b"Subject: =?utf-8?q?=5BOntario_Builder=5D?= \n \
+             and the subject continues",
+        )
+        .unwrap();
+        assert_eq!(
+            parsed.get_value().unwrap(),
+            "[Ontario Builder] and the subject continues"
         );
 
         let (parsed, _) = parse_header(b"Subject: =?ISO-2022-JP?B?GyRCRnwbKEI=?=\n\t=?ISO-2022-JP?B?GyRCS1wbKEI=?=\n\t=?ISO-2022-JP?B?GyRCOGwbKEI=?=")