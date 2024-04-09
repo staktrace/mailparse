595888b12fa6454e1c4630930d22fbd396c9ffc0
Bill Thiede <git@xinu.tv>
2024-04-09 07:33:32 -0700

    Use BASE64_MIME_PERMISSIVE to allow trailing bits in b64 data
    This uses the updated data-encoding crate that introduced
    the new functionality in https://github.com/ia0/data-encoding/pull/104

--- src/body.rs
+++ src/body.rs
@@ -1,5 +1,6 @@
-use crate::{MailParseError, ParsedContentType};
 use charset::{decode_ascii, Charset};
+
+use crate::{MailParseError, ParsedContentType};
 
 /// Represents the body of an email (or mail subpart)
 pub enum Body<'a> {
@@ -141,7 +142,7 @@ fn decode_base64(body: &[u8]) -> Result<Vec<u8>, MailParseError> {
         .filter(|c| !c.is_ascii_whitespace())
         .cloned()
         .collect::<Vec<u8>>();
-    Ok(data_encoding::BASE64_MIME.decode(&cleaned)?)
+    Ok(data_encoding::BASE64_MIME_PERMISSIVE.decode(&cleaned)?)
 }
 
 fn decode_quoted_printable(body: &[u8]) -> Result<Vec<u8>, MailParseError> {
